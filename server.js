e85f8fe248a4071b389fc95b3c0853b8c63518c0
roshankumar1724 <roshankumar1724@gmail.com>
2016-09-25 09:47:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+    res.send('Hey !! I\'m in Article one');
+});
+
+app.get('/article-two', function (req, res) {
+    res.send('Hey !! I\'m in Article two');
+});
+
+app.get('/article-three', function (req, res) {
+    res.send('Hey !! I\'m in Article three');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
